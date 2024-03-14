f3ed20ec5ec1811c6a20a8ad77587247a343ec04
Olexander Bryl <edlrian814@gmail.com>
2024-03-14 22:05:00 +0200

    Set up alembic

--- settings.py
+++ settings.py
@@ -5,8 +5,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-POSTGRES_DATABASE = os.getenv("POSTGRES_DATABASE")
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_DATABASE_URL = os.getenv("POSTGRES_DATABASE_URL")
