70c94b0b9fe815dcc1a64911d733c890124b6c3b
Olexander Bryl <edlrian814@gmail.com>
2024-03-24 19:25:21 +0200

    Add validate_password func

--- auth/utils.py
+++ auth/utils.py
@@ -25,3 +25,7 @@ def decode_jwt(
 
 def hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def validate_password(password: str, hashed_password: bytes) -> bool:
+    return bcrypt.checkpw(password.encode(), hashed_password)
