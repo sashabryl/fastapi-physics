15b62f53d8b9608fa8c97902589063458a41fbe3
Olexander Bryl <edlrian814@gmail.com>
2024-03-24 19:14:43 +0200

    Add RSA public & private keys & package auth & encode_jwt and decode_jwt funcs

---
+++ auth/utils.py
@@ -0,0 +1,22 @@
+import jwt
+from jwt import encode, decode
+from settings import PUBLIC_KEY, PRIVATE_KEY
+
+
+ALGORITHM = "RS256"
+
+
+def encode_jwt(
+        payload: dict,
+        key: str = PRIVATE_KEY,
+        algorithm: str = ALGORITHM
+) -> str:
+    return encode(payload=payload, key=PRIVATE_KEY, algorithm=algorithm)
+
+
+def decode_jwt(
+        token: str | bytes,
+        key: str = PUBLIC_KEY,
+        algorithm: str = ALGORITHM
+) -> dict:
+    return jwt.decode(jwt=token, key=key, algorithms=[algorithm])
--- settings.py
+++ settings.py
@@ -11,3 +11,6 @@ AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_CUSTOM_DOMAIN = "https://physics-s3.s3.eu-north-1.amazonaws.com/"
+
+PRIVATE_KEY = os.getenv("PRIVATE_KEY")
+PUBLIC_KEY = os.getenv("PUBLIC_KEY")
