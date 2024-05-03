499e68bebff7afef4dc68435d88c67337849d561
Olexander Bryl <edlrian814@gmail.com>
2024-05-03 21:24:15 +0300

    Add anyio_backend fixture

--- conftest.py
+++ conftest.py
@@ -7,6 +7,11 @@ from main import app
 
 
 @pytest.fixture()
+def anyio_backend() -> str:
+    return "asyncio"
+
+
+@pytest.fixture()
 async def client() -> AsyncIterator[AsyncClient]:
     async with AsyncClient(app=app, base_url="http://testserver") as client:
         yield client
